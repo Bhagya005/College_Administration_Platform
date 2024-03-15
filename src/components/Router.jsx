acab3ccc15ec0c1800e59f8744ec3fe31cad6e48
jiya <jiahofficial64@gmail.com>
2024-03-15 21:48:22 +0530

    Login modified

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import SelectionPage from "../pages/SelectionPage.jsx";
-import Login from "../pages/Login";
-
+import Login from "../pages/Login.jsx";
 export function RouterPaths() {
   
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<SelectionPage />} />
+
     </Routes>
   );
 }
